92d8bfde75181d95ed984f016335b32d8f5c249c
Eugene <eugene.sidorenko@gmail.com>
2018-03-09 20:45:21 +0200

    test

--- Python udacity/my-first-script.py
+++ Python udacity/my-first-script.py
@@ -5,3 +5,11 @@ def welcome_message(name):
 #Call the welcome message function with the input "Udacity Student" 
 #and print the output
 print(welcome_message("Udacity Student"))
+
+
+sdf
+saf
+saff
+asf
+asf
+saf
